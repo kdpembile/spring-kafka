4203c77fa58abe67fdda32a04b22dc358e56b7f0
KD <kdpembile@securitybank.com.ph>
2024-05-09 00:37:51 +0800

    Initial commit

---
+++ demo-for-kafka/src/main/java/com/kentisthebest/DemoForKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForKafkaApplication.class, args);
+	}
+
+}
